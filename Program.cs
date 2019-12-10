91d09373a0139e4781833452d35991771330eaa6
Matthew Wood <matthew.wood@nationwide.co.uk>
2019-12-10 15:36:00 +0000

    Added new feature 'A'

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from the wonderful world of git!!");
+
+            // this work is done as part of feature 'A'
+            Console.WriteLine("Hello from feature 'A'");
         }
     }
 }
